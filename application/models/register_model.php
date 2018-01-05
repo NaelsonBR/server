56074cd36fa7320806b1269903022962fdcaef7f
NaelsonBr <naelson321@gmail.com>
2018-01-05 21:09:01 -0200

    Organizetion systax

--- application/models/register_model.php
+++ application/models/register_model.php
@@ -1,8 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class register_model extends CI_Model
-{
+class register_model extends CI_Model{
+
     function __construct()
     {
         parent::__construct();
@@ -13,18 +13,21 @@ class register_model extends CI_Model
     {
         $bool = false;
         $data = array(
-            'account' => $ac,
-            'password' => $pw,
-            'email' => $email,
+            'account'      => $ac,
+            'password'     => $pw,
+            'email'        => $email,
             'access_level' => $access,
-            'status' => $stat,
-            'data_create' => $datCreate,
-            'data_log' => $datLog
+            'status'       => $stat,
+            'data_create'  => $datCreate,
+            'data_log'     => $datLog
         );
+
         try {
             $bool = true;
             $this->db->insert('user', $data);
+
         } catch (Exception $e) {
+
             return $bool = false;
         }
 
