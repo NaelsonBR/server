823e9dbdf092862582b32585d181aeab73546d03
NaelsonBr <naelson321@gmail.com>
2018-01-09 11:19:38 -0200

    Update menu bg for menu responsive and bg botton

--- assets/js/global.js
+++ assets/js/global.js
@@ -2,9 +2,22 @@
 
 $(function () {
      $('#chevron_left').bind('click',function () {
-         if($('.list-menu').css('width','50px')){
+         if($('.list-menu').css('width','55px').css('transition','1s') &&
+             $('.menu-left').css('width','55px').css('transition','1s')){
+
              $('.letters-all-menu').css('visibility','hidden');
+
          }
 
      });
+
+    $('#chevron_right').bind('click',function () {
+        if($('.list-menu').css('width','15%').css('transition','0s') &&
+            $('.menu-left').css('width','15%').css('transition','0s')){
+            
+            $('.letters-all-menu').css('visibility','visible');
+
+        }
+
+    });
 });
