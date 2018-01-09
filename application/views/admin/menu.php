f1018534775f9271cf6568b4233dc6fd11d4ef7b
NaelsonBr <naelson321@gmail.com>
2018-01-09 10:51:21 -0200

    Fix nameclature

--- application/views/admin/menu.php
+++ application/views/admin/menu.php
@@ -2,16 +2,16 @@
     <ul class="list-menu">
 
 
-        <li><a href="#"> <button type="button" class="btn-menu"><i class="fa fa-tachometer menu-icon" aria-hidden="true"></i><span class="word">DashBoard</span></button></a></li>
+        <li><a href="#"> <button type="button" class="btn-menu"><i class="fa fa-tachometer menu-icon" aria-hidden="true"></i><span class="letters-all-menu">DashBoard</span></button></a></li>
         <li class="li-menu-modified1"></li>
         <li class="li-menu-modified2"></li>
-        <li><a href="#"> <button type="button" class="btn-menu"><i class="fa fa-user-circle menu-icon" aria-hidden="true"></i><span class="word">Account</span></button></a></li>
+        <li><a href="#"> <button type="button" class="btn-menu"><i class="fa fa-user-circle menu-icon" aria-hidden="true"></i><span class="letters-all-menu">Account</span></button></a></li>
         <li class="li-menu-modified1"></li>
         <li class="li-menu-modified2"></li>
-        <li><a href="#"> <button type="button" class="btn-menu"><i class="fa fa-line-chart menu-icon" aria-hidden="true"></i><span class="word">Statistic</span></button></a></li>
+        <li><a href="#"> <button type="button" class="btn-menu"><i class="fa fa-line-chart menu-icon" aria-hidden="true"></i><span class="letters-all-menu">Statistic</span></button></a></li>
         <li class="li-menu-modified1"></li>
         <li class="li-menu-modified2"></li>
-        <li><a href="#"> <button type="button" class="btn-menu"><i class="fa fa-eye menu-icon" aria-hidden="true"></i><span class="word">View</span></button></a></li>
+        <li><a href="#"> <button type="button" class="btn-menu"><i class="fa fa-eye menu-icon" aria-hidden="true"></i><span class="letters-all-menu">View</span></button></a></li>
 
     </ul>
 </nav>
--- assets/js/global.js
+++ assets/js/global.js
@@ -3,7 +3,7 @@
 $(function () {
      $('#chevron_left').bind('click',function () {
          if($('.list-menu').css('width','50px')){
-             $('.word').css('visibility','hidden');
+             $('.letters-all-menu').css('visibility','hidden');
          }
 
      });
