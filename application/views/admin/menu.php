24a6e95d3b468ea91dcf67cd518f12a10f102ef6
NaelsonBr <naelson321@gmail.com>
2018-01-10 14:01:32 -0200

    Updates nav menu

--- application/views/admin/menu.php
+++ application/views/admin/menu.php
@@ -1,14 +1,26 @@
+<style>
+    .menu-left{  background-color: #2c3e50; }
+
+
+</style>
 <nav class="menu-left">
     <ul class="list-menu">
 
 
-        <li><a href="#"> <button type="button" class="btn-menu"><i class="fa fa-tachometer menu-icon" aria-hidden="true"></i><span class="letters-all-menu">DashBoard</span></button></a></li>
+        <li><a href="#"> <button type="button" class="btn-menu"><i class="fa fa-tachometer menu-icon" aria-hidden="true"></i><span class="letters-all-menu">DashBoard</span></button></a>
+        <ul class="dashboard-list">
+            <li><a href="#"> <button type="button" class="btn-menu-dashboard"><i class="fa fa-user-circle menu-icon-dashboard" aria-hidden="true"></i><span class=" letters-all-menu-dashboard">Account</span></button></a></li>
+            <li class="li-menu-modified1"></li>
+            <li class="li-menu-modified2"></li>
+            <li><a href="#"> <button type="button" class="btn-menu-dashboard"><i class="fa fa-line-chart menu-icon-dashboard" aria-hidden="true"></i><span class="letters-all-menu-dashboard">Statistic</span></button></a></li>
+        </ul>
+        </li>
         <li class="li-menu-modified1"></li>
         <li class="li-menu-modified2"></li>
-        <li><a href="#"> <button type="button" class="btn-menu"><i class="fa fa-user-circle menu-icon" aria-hidden="true"></i><span class="letters-all-menu">Account</span></button></a></li>
+        <li><a href="#"> <button type="button" class="btn-menu"><i class="fa fa-pencil menu-icon" aria-hidden="true"></i><span class="letters-all-menu">Posts</span></button></a></li>
         <li class="li-menu-modified1"></li>
         <li class="li-menu-modified2"></li>
-        <li><a href="#"> <button type="button" class="btn-menu"><i class="fa fa-line-chart menu-icon" aria-hidden="true"></i><span class="letters-all-menu">Statistic</span></button></a></li>
+        <li><a href="#"> <button type="button" class="btn-menu"><i class="fa fa-comment-o menu-icon" aria-hidden="true"></i><span class="letters-all-menu">Comentarios</span></button></a></li>
         <li class="li-menu-modified1"></li>
         <li class="li-menu-modified2"></li>
         <li><a href="#"> <button type="button" class="btn-menu"><i class="fa fa-eye menu-icon" aria-hidden="true"></i><span class="letters-all-menu">View</span></button></a></li>
--- assets/js/global.js
+++ assets/js/global.js
@@ -1,23 +1,35 @@
-//
+// Naelson.g.saraiva@gmail.com
 
 $(function () {
      $('#chevron_left').bind('click',function () {
-         if($('.list-menu').css('width','55px').css('transition','1s') &&
-             $('.menu-left').css('width','55px').css('transition','1s')){
+         if($('.list-menu').css('width','55px').css('transition','1s') && $('.menu-left').css('width','55px').css('transition','1s')
 
+         && $('.list-menu-dashboard').css('width','55px').css('transition','1s') && $('.menu-left-dashboard').css('width','55px').css('transition','1s')){
              $('.letters-all-menu').css('visibility','hidden');
 
          }
+         $('.letters-all-menu-dashboard').css('visibility','hidden');
+         $('.dashboard-list').addClass('dashboard-list-javascript');
+
+         $('.dashboard-list-javascript').hover(function () {
+             $('.letters-all-menu-dashboard').css('visibility','visible');
+
+         })
 
      });
 
     $('#chevron_right').bind('click',function () {
-        if($('.list-menu').css('width','15%').css('transition','0s') &&
-            $('.menu-left').css('width','15%').css('transition','0s')){
-            
-            $('.letters-all-menu').css('visibility','visible');
 
+        var n = 0;
+        if($('.list-menu').css('width','15%').css('transition','0s') && $('.menu-left').css('width','15%').css('transition','0s')
+
+            && $('.list-menu-dashboard').css('width','15%').css('transition','0s') && $('.menu-left-dashboard').css('width','15%').css('transition','0s')){
+            $('.letters-all-menu').css('visibility','visible');
         }
+
+        $('.letters-all-menu-dashboard').css('visibility','visible');
+        $('.dashboard-list').removeClass('dashboard-list-javascript').addClass('dashboard-list');
 
     });
 });
+
